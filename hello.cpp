95f8c20d9d67c3263d0c0ec5f4a107853080e611
Chano363 <3334309819@qq.com>
2025-03-07 22:39:26 +0800

    Create test function for Add function

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
+void TestAdd()
+{
+	int a, b;
+	cin >> a >> b;
+	if (Add(a, b) == a + b)
+	{
+		cout << "Success!" << endl;
+	}
+}
 void PrintHello()
 {
 	cout << "hello!" << endl;
